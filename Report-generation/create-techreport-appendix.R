46fd85e2c86675bb0a5f448d0cf80b7c6d112460
gomezcatalina <gomezcatalina@gmail.com>
2020-12-02 10:22:32 -0400

    Finished layouts for ticket class L (two pager per species).

--- Report-generation/create-techreport-appendix.R
+++ Report-generation/create-techreport-appendix.R
@@ -22,16 +22,18 @@ temp <- lapply(taxo.final$species.code, function(x) {
   spp_file1 <- paste0("SS",x,"_IDWMap-biomass.pdf")
   
   spp_file2 <- paste0("SS",x,"_Stratified-biomass.pdf")
-  #spp_file3 <- paste0("SS",x,"_Distribution-usingbiomass.pdf")
-  #spp_file4 <- paste0("SS",x,"_BDcorrelations.pdf")
+  spp_file3 <- paste0("SS",x,"_Distribution-usingbiomass.pdf")
+  spp_file4 <- paste0("SS",x,"_BDcorrelations.pdf")
   
-  #spp_file5 <- paste0("SS",x,"LengthFreq-NAFO.pdf")
-  #spp_file6 <- paste0("SS",x,"Condition4X4VW.pdf")
+  spp_file5 <- paste0("SS",x,"_LengthFreq-NAFO.pdf")
   
-  #spp_file7 <- paste0("SS",x,"DepthDist.pdf")
-  #spp_file8 <- paste0("SS",x,"TemperatureDist.pdf")
-  #spp_file9 <- paste0("SS",x,"SalinityDist.pdf")
-  #spp_file10 <- paste0("SS",x,"DDHSslopes.pdf")
+  spp_file6 <- paste0("SS",x,"_Condition4X4VW.pdf")
+  
+  spp_file7 <- paste0("SS",x,"_DepthDist.pdf")
+  spp_file8 <- paste0("SS",x,"_TemperatureDist.pdf")
+  spp_file9 <- paste0("SS",x,"_SalinityDist.pdf")
+  
+  spp_file10 <- paste0("SS",x,"_DDHSslopes.pdf")
   
   latin_name <- taxo.final$scientificname[taxo.final$species.code == x]
   english_name <- taxo.final$comm.english[taxo.final$species.code == x]
@@ -50,11 +52,12 @@ temp <- lapply(taxo.final$species.code, function(x) {
   out[[i]] <- paste0(
     "Scientific name: [", latin_name, "](",worms_link,") \n \\newline")
   i <- i + 1
+  #Figure 1
   out[[i]] <- "\\begin{minipage}{1.0\\textwidth}"
   i <- i + 1
   out[[i]] <- " \\begin{tabular}{c}"
   i <- i + 1
-  out[[i]] <- paste0("\\includegraphics[width=6in]{../Figures-Actual/",
+  out[[i]] <- paste0("\\includegraphics[width=5.5in]{../Figures-Actual/",
                      spp_file1, "} \\\\ ")
   i <- i + 1
   out[[i]] <- "\\end{tabular} "
@@ -65,21 +68,101 @@ temp <- lapply(taxo.final$species.code, function(x) {
   i <- i + 1
   #end of Figure 1
   
-  #Figure 2,3 and 4
+  #Figures 2,3 and 4
   out[[i]] <- "\\begin{minipage}{1.0\\textwidth}"
   i <- i + 1
-  out[[i]] <- " \\begin{tabular}{c}"
+  out[[i]] <- " \\begin{tabular}{ccc}"
   i <- i + 1
   out[[i]] <- paste0("\\includegraphics[width=1.8in]{../Figures-Actual/",
-                     spp_file2, "} \\\\ ")
+                      spp_file2, "} & ")
+  i <- i + 1
+  out[[i]] <- paste0("\\includegraphics[width=1.8in]{../Figures-Actual/",
+                     spp_file3, "} & ")
+  i <- i + 1
+  out[[i]] <- paste0("\\includegraphics[width=1.8in]{../Figures-Actual/",
+                     spp_file4, "} \\\\ ")
   i <- i + 1
   out[[i]] <- "\\end{tabular} "
   i <- i + 1
-  out[[i]] <- paste0("\\captionof{figure}{Inverse distance weighted distribution of catch biomass (kg/tow) for ", english_name,".}")
+  out[[i]] <- paste0("\\captionof{figure}{Stratified random estimates of biomass (kg/tow), D75 and D95 and the correlation between D75 and
+biomass of ", english_name,".}")
   i <- i + 1
   out[[i]] <- "\\end{minipage} \n"
   i <- i + 1
   #end of Figure 2, 3 and 4
+  
+  #Figures 7,8 and 9
+  out[[i]] <- "\\begin{minipage}{1.0\\textwidth}"
+  i <- i + 1
+  out[[i]] <- " \\begin{tabular}{ccc}"
+  i <- i + 1
+  out[[i]] <- paste0("\\includegraphics[width=1.8in]{../Figures-Actual/",
+                     spp_file7, "} & ")
+  i <- i + 1
+  out[[i]] <- paste0("\\includegraphics[width=1.8in]{../Figures-Actual/",
+                     spp_file8, "} & ")
+  i <- i + 1
+  out[[i]] <- paste0("\\includegraphics[width=1.8in]{../Figures-Actual/",
+                     spp_file9, "} \\\\ ")
+  i <- i + 1
+  out[[i]] <- "\\end{tabular} "
+  i <- i + 1
+  out[[i]] <- paste0("\\captionof{figure}{Catch distribution by depth, temperature and salinity of ", english_name,".}")
+  i <- i + 1
+  out[[i]] <- "\\end{minipage} \n"
+  i <- i + 1
+  #end of Figure 7,8 and 9
+  
+  #Figure 5 
+  out[[i]] <- "\\begin{minipage}{1.0\\textwidth}"
+  i <- i + 1
+  out[[i]] <- " \\begin{tabular}{c}"
+  i <- i + 1
+  out[[i]] <- paste0("\\includegraphics[width=5in]{../Figures-Actual/",
+                     spp_file5, "} \\\\ ")
+  i <- i + 1
+  out[[i]] <- "\\end{tabular} "
+  i <- i + 1
+  out[[i]] <- paste0("\\captionof{figure}{Length frequency distribution in NAFO units 4X and 4VW for ", english_name,".}")
+  i <- i + 1
+  out[[i]] <- "\\end{minipage} \n"
+  i <- i + 1
+  #end of Figure 5 
+  out[[i]] <- "\n"
+  #Figure 6 
+  out[[i]] <- "\\begin{minipage}{1.0\\textwidth}"
+  i <- i + 1
+  out[[i]] <- " \\begin{tabular}{c}"
+  i <- i + 1
+  out[[i]] <- paste0("\\includegraphics[width=5in]{../Figures-Actual/",
+                     spp_file6, "} \\\\ ")
+  i <- i + 1
+  out[[i]] <- "\\end{tabular} "
+  i <- i + 1
+  out[[i]] <- paste0("\\captionof{figure}{Average fish condition in NAFO units 4X and 4VW for ", english_name,".}")
+  i <- i + 1
+  out[[i]] <- "\\end{minipage} \n"
+  i <- i + 1
+  #end of Figure 6
+  
+  #Figure 10
+  out[[i]] <- "\\begin{minipage}{1.0\\textwidth}"
+  i <- i + 1
+  out[[i]] <- " \\begin{tabular}{c}"
+  i <- i + 1
+  out[[i]] <- paste0("\\includegraphics[width=5in]{../Figures-Actual/",
+                     spp_file10, "} \\\\ ")
+  i <- i + 1
+  out[[i]] <- "\\end{tabular} "
+  i <- i + 1
+  out[[i]] <- paste0("\\captionof{figure}{DDHS slopes versus median stratum abundance. The last two digits of each stratum number is shown in
+the figure for ", english_name,".}")
+  i <- i + 1
+  out[[i]] <- "\\end{minipage} \n"
+  i <- i + 1
+  #end of Figure 10
+  out[[i]] <- "\n"
+  
   
   #End of Figures:
   out[[i]] <- "\\clearpage\n"
@@ -90,8 +173,8 @@ temp <- lapply(temp, function(x) paste(x, collapse = "\n"))
 temp <- paste(temp, collapse = "\n")
 temp <- c("# Appendix\n<!-- This page has been automatically generated: do not edit by hand -->\n", temp, "\\printindex \n")
 if (!exists("N"))
-  writeLines(temp, con = file.path("report-EN", "plot-pages.Rmd"), useBytes=T)
-  #writeLines(temp, "plot-pages.Rmd", useBytes=T)
+  #writeLines(temp, con = file.path("report-EN", "plot-pages.Rmd"), useBytes=T)
+  writeLines(temp, "plot-pages.Rmd", useBytes=T)
 ## French Tech Report
 
 
